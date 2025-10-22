7f8445ab2299ef05e639485fb70e070217851d37
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2025-10-22 14:20:24 +0200

    Update TVRemote.java

--- src/main/java/com/campusdual/classroom/TVRemote.java
+++ src/main/java/com/campusdual/classroom/TVRemote.java
@@ -2,4 +2,51 @@ package com.campusdual.classroom;
 
 public class TVRemote {
 
+    int channel;
+    int volume;
+    boolean on;
+    String color;
+
+    public TVRemote(String color) {
+        this.channel = 0;
+        this.volume = 20;
+        this.color = color;
+    }
+
+    public boolean turnOn() {
+
+    }
+
+    public boolean turnOff() {
+
+    }
+
+    public void channelUp() {
+
+    }
+
+    public void channelDown() {
+
+    }
+
+    public void volumeUp() {
+      
+    }
+
+    public void volumeDown() {
+
+    }
+
+    public String getColor() {
+
+    }
+
+    private boolean checkMinValue(int value) {
+        if (value == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
