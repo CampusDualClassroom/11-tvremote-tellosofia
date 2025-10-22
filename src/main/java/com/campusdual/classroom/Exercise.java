787f9415c098a159fcfb719264800fc7fdb98325
Sofía Tello <tellosofia@outlook.com>
2025-10-22 19:04:01 +0200

    Exercise 11: Solved

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -6,5 +6,18 @@ public class Exercise {
         TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
+        redRemote.turnOn();
+        redRemote.channelUp();
+        redRemote.channelDown();
+        redRemote.channelDown();
+        redRemote.volumeUp();
+        redRemote.volumeDown();
+
+        for (int i = 0; i < 25; i++) {
+            redRemote.volumeDown();
+        }
+
+        System.out.println(redRemote.getColor());
+        redRemote.turnOff();
     }
 }
--- src/main/java/com/campusdual/classroom/TVRemote.java
+++ src/main/java/com/campusdual/classroom/TVRemote.java
@@ -1,44 +1,62 @@
 package com.campusdual.classroom;
 
 public class TVRemote {
-
     int channel;
     int volume;
-    boolean on;
+    boolean on = false;
     String color;
 
+    // constructor -> alt + insert -> default -> on queda por defecto en false
     public TVRemote(String color) {
         this.channel = 0;
         this.volume = 20;
         this.color = color;
     }
 
+    // métodos: public + tipo de dato + nombre
     public boolean turnOn() {
-
+        this.on = true;
+        System.out.println("Welcome!");
+        return this.on;
     }
 
-    public boolean turnOff() {
 
+    public boolean turnOff() {
+        this.on = false;
+        System.out.println("Bye bye!");
+        return this.on;
     }
 
     public void channelUp() {
-
+        this.channel++;
+        System.out.println("Channel: " + this.channel);
     }
 
     public void channelDown() {
-
-    }
+        if (!checkMinValue(this.channel)){
+            System.out.println("You're already in channel 0");
+        }else {
+            this.channel--;
+            System.out.println("Channel: " + this.channel);
+        }
+        }
 
     public void volumeUp() {
-      
+        this.volume++;
+        System.out.println("Volume: " + this.volume);
     }
 
     public void volumeDown() {
-
+        if (!checkMinValue(this.volume)){
+            System.out.println("You're on mute");
+        }else {
+            this.volume--;
+            System.out.println("Volume: " + this.volume);
+        }
     }
 
     public String getColor() {
-
+        return this.color;
     }
 
     private boolean checkMinValue(int value) {
